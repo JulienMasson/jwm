db71f27a807f3092f851a82ff6f578d25e23db36
Julien Masson <massonju.eseo@gmail.com>
2018-05-04 08:59:01 -0700

    test: add conf test
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

---
+++ test/conf-test.c
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the jwm distribution:
+ * https://github.com/JulienMasson/jwm
+ *
+ * Copyright (c) 2018 Julien Masson.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "core.h"
+#include "conf.h"
+
+
+CORE_TEST_START(conf_read_fail)
+{
+    fail_unless(conf_read() == -1, "No config set, should failed");
+}
+CORE_TEST_END(conf_read_fail);
+
+
+CORE_TEST_START(conf_read_fail2)
+{
+    char conf_path[] = "asdasdasd";
+    conf_init(conf_path);
+    fail_unless(conf_read() == -1, "No config set, should failed");
+}
+CORE_TEST_END(conf_read_fail2);
+
+
+CORE_TEST_START(conf_read_pass)
+{
+    char conf_path[] = "/home/julienm/bin/jwm-repo/res/.jwmrc";
+    conf_init(conf_path);
+    fail_unless(conf_read() == 0, "Cannot read the config");
+}
+CORE_TEST_END(conf_read_pass);
